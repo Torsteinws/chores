ecdcd9f119eae56226b9a0514ceea8fdbd6580bd
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-07-04 03:27:09 +0700

    [WIP] Parse input args and read all lines in source.txt

--- Program.cs
+++ Program.cs
@@ -1,9 +1,45 @@
 ﻿namespace ImgCopy;
 
+using System.IO;
+
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        var (sourceFile, destination) = ParseArgs(args);
+        var files = ParseFile(sourceFile.FullName);
+            
+    }
+
+    private static (FileInfo, DirectoryInfo) ParseArgs(string[] args) {
+        if (args.Length < 2) {
+            Console.WriteLine("Usage: imgcopy <source> <destination>");
+            Environment.Exit(1);
+        }
+
+        FileInfo sourceFile = new(args[0].Trim());
+        DirectoryInfo destination = new(args[1].Trim());
+
+        if (!sourceFile.Exists) {
+            Console.WriteLine("Source file does not exist.");
+            Environment.Exit(1);
+        }
+
+        if (!destination.Exists) {
+            Directory.CreateDirectory(destination.FullName);
+        }
+
+        return (sourceFile, destination);
+    }
+
+    private static List<string> ParseFile(string path) {
+        var paths = new List<string>();
+        using (var reader = new StreamReader(path)) {
+            string? line = reader.ReadLine();
+            while (line is not null) {
+                paths.Add(line.Trim());
+            }
+        }
+        return paths;
     }
 }
