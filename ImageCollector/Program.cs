feabb16b14255d49547450eb5081fdc78d06d4fe
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-07-04 12:34:13 +0700

    Recursively find all image files

--- ImageCollector/Program.cs
+++ ImageCollector/Program.cs
@@ -1,9 +1,28 @@
 ﻿namespace ImageCollector;
 
+using System.IO;
+
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Image Collector");
+        DirectoryInfo rootDir = new(Directory.GetCurrentDirectory());
+        foreach(var file in GetImageFiles(rootDir))
+        {
+            Console.WriteLine(file.FullName);
+        }
     }
+
+
+    static IEnumerable<FileInfo> GetImageFiles(DirectoryInfo dir)
+    {
+        var extensions = new[] { ".jpg", ".jpeg", ".png", ".bmp", ".gif", ".mp4", ".mov", ".webp", ".heic", ".tiff" };
+
+        var files = dir
+            .GetFiles("*.*", SearchOption.AllDirectories)
+            .Where(file => extensions.Contains(file.Extension.ToLower()));
+
+        return files;        
+    }
+
 }
